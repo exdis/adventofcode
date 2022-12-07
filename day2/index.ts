9bc12abe1d050933746654d502e67f01f5593e0c
Denis Kolesnikov <exsdis@gmail.com>
2022-12-07 14:48:42 +0100

    Day 2 solution (part 2)

--- day2/index.ts
+++ day2/index.ts
@@ -5,17 +5,24 @@ type Item = 'A' | 'B' | 'C' | 'X' | 'Y' | 'Z';
 const WIN_SCORE = 6;
 const DRAW_SCORE = 3;
 
-const scoreMap: Map<Item, { wins: Item, draw: Item, score: number }> = new Map([
-    ['A', { wins: 'Z', draw: 'X', score: 1 }],
-    ['B', { wins: 'X', draw: 'Y', score: 2 }],
-    ['C', { wins: 'Y', draw: 'Z', score: 3 }],
-    ['X', { wins: 'C', draw: 'A', score: 1 }],
-    ['Y', { wins: 'A', draw: 'B', score: 2 }],
-    ['Z', { wins: 'B', draw: 'C', score: 3 }]
+const scoreMap: Map<Item, {
+    wins: Item,
+    draw: Item,
+    loose: Item,
+    score: number,
+    strategy?: string
+}> = new Map([
+    ['A', { wins: 'Z', draw: 'X', loose: 'Y', score: 1 }],
+    ['B', { wins: 'X', draw: 'Y', loose: 'Z', score: 2 }],
+    ['C', { wins: 'Y', draw: 'Z', loose: 'X', score: 3 }],
+    ['X', { wins: 'C', draw: 'A', loose: 'B', score: 1, strategy: 'loose' }],
+    ['Y', { wins: 'A', draw: 'B', loose: 'C', score: 2, strategy: 'draw' }],
+    ['Z', { wins: 'B', draw: 'C', loose: 'A', score: 3, strategy: 'win' }]
 ])
 
 export const run = () => {
     let score = 0;
+    let alternativeScore = 0;
 
     const input = getInputLines(2);
 
@@ -30,6 +37,21 @@ export const run = () => {
 
         score += scoreMapItem.score;
 
+        switch (scoreMapItem.strategy) {
+            case 'draw':
+                alternativeScore += DRAW_SCORE;
+                console.log('###', scoreMap.get(opponent).draw);
+                alternativeScore += scoreMap.get(scoreMap.get(opponent).draw).score;
+                break;
+            case 'win':
+                alternativeScore += WIN_SCORE;
+                alternativeScore += scoreMap.get(scoreMap.get(opponent).loose).score;
+                break;
+            case 'loose':
+                alternativeScore += scoreMap.get(scoreMap.get(opponent).wins).score;
+                break;
+        }
+
         if (scoreMapItem.draw === opponent) {
             score += DRAW_SCORE;
         }
@@ -40,5 +62,6 @@ export const run = () => {
     }
 
     console.log('Score:', score);
+    console.log('AlternativeScore:', alternativeScore);
 };
 
