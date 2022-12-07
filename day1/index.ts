cf72f6397d0f13e40876fa620ab49e2baddb79d4
Denis Kolesnikov <exsdis@gmail.com>
2022-12-07 14:29:58 +0100

    Day 1 solution (part 2)

--- day1/index.ts
+++ day1/index.ts
@@ -6,6 +6,8 @@ export const run = () => {
     let max = 0;
     let sum = 0;
 
+    const calories = [];
+
     for (const item of input) {
         if (item !== '') {
             sum += parseInt(item, 10);
@@ -13,10 +15,14 @@ export const run = () => {
             if (sum > max) {
                 max = sum;
             }
+            calories.push(sum);
             sum = 0;
         }
     }
 
+    calories.sort((a, b) => b - a);
+
     console.log('max:', max);
+    console.log('top 3:', calories[0] + calories[1] + calories[2]);
 }
 
