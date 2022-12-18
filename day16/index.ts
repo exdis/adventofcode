7a84f3cb47c99b48f7b5d7e3302d5552c4804ad1
Denis Kolesnikov <exsdis@gmail.com>
2022-12-18 11:24:41 +0100

    Day 16 solution (part 2) not working

--- day16/index.ts
+++ day16/index.ts
@@ -62,7 +62,6 @@ export const run = () => {
         current: valveIndex.get('AA').node,
         nextNodes: [...valveIndex.values()].filter(item => item.positive).map(item => item.node),
         timeLeft: TIME,
-        steps: [],
         pressure: 0
     }];
 
@@ -85,7 +84,6 @@ export const run = () => {
                 current: node,
                 nextNodes: path.nextNodes.filter(item => item !== node),
                 timeLeft: path.timeLeft - distances.get(node) - 1,
-                steps: [...path.steps, node],
                 pressure: path.pressure + (path.timeLeft - distances.get(node) - 1) * node.value
             });
         }
@@ -96,4 +94,65 @@ export const run = () => {
     }
 
     console.log('Result:', maxPressure);
+
+    const paths2 = [{
+        current1: valveIndex.get('AA').node,
+        current2: valveIndex.get('AA').node,
+        nextNodes: [...valveIndex.values()].filter(item => item.positive).map(item => item.node),
+        timeLeft1: TIME_PART_2,
+        timeLeft2: TIME_PART_2,
+        pressure: 0
+    }];
+
+    let maxPressure2 = 0;
+
+    let n = 0;
+    while (paths2.length) {
+        const path = paths2.shift();
+
+        const distances1 = distanceMap(path.current1);
+        const distances2 = distanceMap(path.current2);
+
+        let moved = false;
+
+        const nextNodes: [TreeNode, TreeNode][] = path.nextNodes.reduce((res, item) => {
+            for (const node of path.nextNodes) {
+                if (node !== item) {
+                    res.push([node, item]);
+                }
+            }
+            return res;
+        }, []);
+
+        for (const [node1, node2] of nextNodes) {
+            if (node1 === path.current1) continue;
+            if (node2 === path.current2) continue;
+
+            if (path.timeLeft1 - distances1.get(node1) <= 1) continue;
+            if (path.timeLeft1 - distances2.get(node2) <= 1) continue;
+
+            moved = true;
+
+            paths2.push({
+                current1: node1,
+                current2: node2,
+                nextNodes: path.nextNodes.filter(item => item !== node1 && item !== node2),
+                timeLeft1: path.timeLeft1 - distances1.get(node1) - 1,
+                timeLeft2: path.timeLeft2 - distances2.get(node2) - 1,
+                pressure: path.pressure + ((path.timeLeft1 - distances1.get(node1) - 1) * node1.value) + ((path.timeLeft2 - distances2.get(node2) - 1) * node2.value)
+            })
+        }
+
+        if (!moved && path.pressure > maxPressure2) {
+            console.log('*****', path.pressure);
+            maxPressure2 = path.pressure;
+        }
+
+        if (n % 10000 === 0) {
+            console.log(`${n} path processed, ${paths2.length} paths in total`)
+        }
+        n++;
+    }
+
+    console.log('Result:', maxPressure2);
 }
