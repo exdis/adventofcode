51e1361625f4b83e69eb43a7839074dc1f04b245
Denis Kolesnikov <exdis@proton.me>
2025-12-09 21:29:48 +0100

    2025 zig day7 second part

--- 2025/src/day7/main.zig
+++ 2025/src/day7/main.zig
@@ -9,7 +9,7 @@ pub fn run() !void {
     var input = try utils.readInputLines(allocator, "src/day7/input.txt");
     defer input.deinit();
 
-    std.debug.print("{s}", .{input.data});
+    // std.debug.print("{s}", .{input.data});
 
     const startIdx = std.mem.indexOfScalar(u8, input.lines.items[0], 'S') orelse {
         return error.WrongInput;
@@ -20,12 +20,15 @@ pub fn run() !void {
     try rays.resize(allocator, input.lines.items[0].len);
     rays.items[startIdx] = true;
 
-    std.debug.print("Start idx {}\n", .{startIdx});
+    var paths: std.ArrayList(usize) = .{};
+    defer paths.deinit(allocator);
+    try paths.appendNTimes(allocator, 0, input.lines.items[0].len);
+    paths.items[startIdx] = 1;
 
     var result: usize = 0;
+    var result2: usize = 0;
     for (input.lines.items, 0..input.lines.items.len) |line, i| {
         if (i == 0) continue;
-
         for (line, 0..line.len) |char, j| {
             for (0..rays.items.len) |rayIdx| {
                 if (rayIdx == j and rays.items[rayIdx]) {
@@ -33,10 +36,13 @@ pub fn run() !void {
                         rays.items[rayIdx] = false;
                         if (rayIdx < rays.items.len - 1) {
                             rays.items[rayIdx + 1] = true;
+                            paths.items[rayIdx + 1] += paths.items[rayIdx];
                         }
-                        if (rayIdx > 1) {
+                        if (rayIdx > 0) {
                             rays.items[rayIdx - 1] = true;
+                            paths.items[rayIdx - 1] += paths.items[rayIdx];
                         }
+                        paths.items[rayIdx] = 0;
                         result += 1;
                     }
                     // std.debug.print("#### CHAR {c} RAY at line {d} idx {d}\n", .{ char, i, j });
@@ -45,5 +51,10 @@ pub fn run() !void {
         }
     }
 
+    for (paths.items) |num| {
+        result2 += num;
+    }
+
     std.debug.print("Result: {}\n", .{result});
+    std.debug.print("Result 2: {}\n", .{result2});
 }
